46d6ecf454e2d252b1661918e49e8daf6a8eedd6
ozer02 <60714065+ozer02@users.noreply.github.com>
2022-02-13 22:22:46 +0300

    Add project files.

---
+++ AgileManagement.Application/services/sprint/SprintService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgileManagement.Application
+{
+    public class SprintService : ISprintService
+    {
+        public AddSprintResponseDto OnProcess(AddSprintRequestDto request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AgileManagement.Domain/models/Sprint.cs
@@ -0,0 +1,44 @@
+﻿using AgileManagement.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgileManagement.Domain.models
+{
+    public class Sprint:Entity
+    {
+        public string Name { get; private set; }
+        public DateTime StartDate { get; private set; }
+        public DateTime EndDate { get; private set; }
+        public Project Project { get; private set; }
+
+        public Sprint(DateTime startDate, DateTime endDate)
+        {
+            Id = Guid.NewGuid().ToString();
+            SetSprintDate(startDate, endDate);
+        }
+        public void SetSprintName(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new Exception("Ad kısmı boş geçilemez");
+            }
+            Name = name.Trim();
+        }
+        public void SetSprintDate(DateTime startDate, DateTime endDate)
+        {
+            if (startDate > endDate)
+            {
+                throw new Exception("Bitiş tarihi başlangıç tarihinden önce olamaz.");
+            }
+            else if ((endDate - startDate).Days > 14)
+            {
+                throw new Exception("Başlangıç ile bitiş tarihi arası 2 haftadan fazla olamaz.");
+            }
+            StartDate = startDate.Date;
+            EndDate = endDate.Date;
+        }
+    }
+}
