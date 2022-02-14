206006586731a772ae961760f66c9812df32891e
ozer02 <60714065+ozer02@users.noreply.github.com>
2022-02-15 01:50:53 +0300

    Sprint Migration

--- AgileManagement.Persistence.EF/migrations/projectdb/20220214203557_AddSprintNew.Designer.cs
+++ AgileManagement.Persistence.EF/migrations/projectdb/20220214203557_AddSprintNew.Designer.cs
@@ -4,14 +4,16 @@ using AgileManagement.Persistence.EF;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AgileManagement.Persistence.EF.migrations.projectdb
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220214203557_AddSprintNew")]
+    partial class AddSprintNew
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
